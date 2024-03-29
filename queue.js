d3461ee9ae812632b09cdac263b00f961d1b96c5
unknown <hilow0090@gmail.com>
2024-03-28 19:31:18 -0700

    finished

--- queue.js
+++ queue.js
@@ -20,26 +20,43 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    let lastNode = new Node(val);
+    if (this.size === 0) {
+      this.first = lastNode;
+      this.last = lastNode;
+    } else {
+      this.last.next = lastNode;
+      this.last = lastNode;
+    }
+    this.size += 1;
+    return undefined;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    if (this.size === 0) {
+      throw new Error("Queue is already empty");
+    } else if (this.size === 1) {
+      this.last = null;
+    }
+    let out = this.first["val"];
+    this.first = this.first.next;
+    this.size -= 1;
+    return out;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    return this.first["val"];
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return !Boolean(this.size);
   }
 }
 
--- stack.js
+++ stack.js
@@ -20,26 +20,43 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    let firstNode = new Node(val);
+    if (this.size === 0) {
+      this.first = firstNode;
+      this.last = firstNode;
+    } else {
+      firstNode.next = this.first;
+      this.first = firstNode;
+    }
+    this.size += 1;
+    return undefined;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (this.size === 0) {
+      throw new Error("Stack is already empty");
+    } else if (this.size === 1) {
+      this.last = null;
+    }
+    let out = this.first["val"];
+    this.first = this.first.next;
+    this.size -= 1;
+    return out;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first["val"];
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return !Boolean(this.size);
   }
 }
 
